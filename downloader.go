9be93dc8f1a981080d3e1d4c09068c51d6a6a9bc
Liru <theliru@gmail.com>
2015-11-10 23:32:20 -0500

    Changed output so that image names are printed before downloading, rather than after

--- downloader.go
+++ downloader.go
@@ -12,22 +12,16 @@ func downloader(id int, limiter <-chan time.Time, imgChan <-chan Image) {
 	for img := range imgChan {
 
 		os.MkdirAll("downloads/"+img.User, 0755)
-		// parsed, _ := url.Parse(img.Url)
-		// s := strings.Split(parsed.Path, "/")
-
-		// filename := s[len(s)-1]
-
 		filename := path.Base(img.Url)
-
 		pathname := fmt.Sprintf("downloads/%s/%s", img.User, filename)
 
 		if _, err := os.Stat(pathname); os.IsNotExist(err) {
 			<-limiter
+			fmt.Println(img)
 			img.Download()
 		} else { // file already exists. Or another error happened. Screw the latter scenario.
 			atomic.AddUint64(&alreadyExists, 1)
 		}
 
-		fmt.Println(img)
 	}
 }
