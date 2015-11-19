9df7784cee4dbb3fd31b31fb1671510393533508
Liru <theliru@gmail.com>
2015-11-19 09:22:29 -0500

    Added basic framework for update mode

--- downloader.go
+++ downloader.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"path"
-	"sync/atomic"
 	"time"
 )
 
@@ -12,16 +10,10 @@ func downloader(id int, limiter <-chan time.Time, imgChan <-chan Image) {
 	for img := range imgChan {
 
 		os.MkdirAll("downloads/"+img.User, 0755)
-		filename := path.Base(img.Url)
-		pathname := fmt.Sprintf("downloads/%s/%s", img.User, filename)
 
-		if _, err := os.Stat(pathname); os.IsNotExist(err) {
-			<-limiter
-			fmt.Println(img)
-			img.Download()
-		} else { // file already exists. Or another error happened. Screw the latter scenario.
-			atomic.AddUint64(&alreadyExists, 1)
-		}
+		<-limiter
+		fmt.Println(img)
+		img.Download()
 
 	}
 }
