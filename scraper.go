2a1793eabf8c33f57b83abebb764dd72ccc5bba6
Liru <theliru@gmail.com>
2016-03-02 14:09:00 -0500

    Initial refactor and renaming of Blog to TumbleLog, to avoid confusion
    There are two struct types named "Blog": one titlecase and one
    lowercase. The titlecase one stores the JSON deserialization
    results. Renamed it to TumbleLog, since that is one of the first
    fields found in the JSON response.

--- scraper.go
+++ scraper.go
@@ -17,34 +17,6 @@ import (
 	"time"
 )
 
-// A Post is reflective of the JSON used in the tumblr API.
-// It contains a PhotoURL, and, optionally, an array of photos.
-// If Photos isn't empty, it typically contains at least one URL which matches PhotoURL.
-type Post struct {
-	ID            string
-	Type          string
-	PhotoURL      string `json:"photo-url-1280"`
-	Photos        []Post `json:"photos,omitempty"`
-	UnixTimestamp int64  `json:"unix-timestamp"`
-	PhotoCaption  string `json:"photo-caption"`
-
-	// for regular posts
-	RegularBody string `json:"regular-body"`
-
-	// for answer posts
-	Answer string
-
-	// for videos
-	Video        string `json:"video-player"`
-	VideoCaption string `json:"video-caption"` // For links to outside sites.
-}
-
-// A Blog is the outer container for Posts. It is necessary for easier JSON deserialization,
-// even though it's useless in and of itself.
-type Blog struct {
-	Posts []Post `json:"posts"`
-}
-
 var (
 	inlineSearch   = regexp.MustCompile(`(http:\/\/\d{2}\.media\.tumblr\.com\/\w{32}\/tumblr_inline_\w+\.\w+)`) // FIXME: Possibly buggy/unoptimized.
 	videoSearch    = regexp.MustCompile(`"hdUrl":"(.*\/tumblr_\w+)"`)                                           // fuck it
